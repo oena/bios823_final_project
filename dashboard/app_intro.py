92668344667939096f89def1bd6793d1faafd209
Oana Enache <8075503+oena@users.noreply.github.com>
2020-11-18 15:15:14 -0500

    minor fix

--- dashboard/app_intro.py
+++ dashboard/app_intro.py
@@ -15,13 +15,13 @@ def app():
     st.title('Exploring COVID19-related clinical trials in the US and beyond')
     st.header('About this dashboard')
     
-    st.markdown("""The overall goal of the dashboard is twofold; first, to provide a tool to explore COVID-19 related trials happening worldwide, and second, to provide a tool for people in the US to identify trials of interest to them by location. \n
+    st.markdown("The overall goal of the dashboard is twofold; first, to provide a tool to explore COVID-19 related trials happening worldwide, and second, to provide a tool for people in the US to identify trials of interest to them by location. \n
                 We have enabled this by providing: \n
                 
                 1. A display of trials happening worldwide ('Overall trials')
                 2. Trials happening in the United States ('US trials')
                 3. Clustering of Trials by different measures of similarity ('Trials Clustering')
-                4. A prediction model related to the activity level (open or not) of available trials ('Activeness predicting').""")
+                4. A prediction model related to the activity level (open or not) of available trials ('Activeness predicting').")
     
     st.header('About the data')
     st.write("This data is derived from [all COVID-19 related results on clinicaltrials.gov](https://clinicaltrials.gov/ct2/results?cond=COVID-19).")  
