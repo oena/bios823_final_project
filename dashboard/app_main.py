691af541c023073a6ba43359c997f0bca5b5c062
Oana Enache <8075503+oena@users.noreply.github.com>
2020-11-18 15:28:40 -0500

    typo fix

--- dashboard/app_main.py
+++ dashboard/app_main.py
@@ -10,7 +10,7 @@ st.set_page_config(layout="wide")
 
 PAGES = {
     "Homepage": app_intro,
-    "World trials": app_world_trials,
+    "World trials": app_world_trial,
     "U.S. trials": app_us_trial,
     "Clustering trials by similarity": app_cluster,
     "Predicting trials' activity status" : app_predict_activeness
